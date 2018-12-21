941d75e93570d4fb05792e0edcdaadfb339fc283
venhaus <paul@ven.haus>
2018-12-21 12:44:45 -0600

    Add tiny suggestions for smoother christmas experience

--- gutschein.js
+++ gutschein.js
@@ -1,19 +1,28 @@
 function submit()
+{
+    var cd=document.forms["myform"]["code"].value;
+    let hash = simpleHash(cd);
+    if(hash===871)
         {
-            var cd=document.forms["myform"]["code"].value;
-            
-            if(cd=="ABENTEUER2019")
-                {
-                    window.location = "gutschein.html";
-                    alert("Ich liebe dich!");
-                }
-            else if(cd=="ESSENMITFREUNDEN")
-                {
-                    window.location = "essen.html";
-                    alert("Ich liebe dich!");
-                }
-            else
-                {
-                    alert("The code you've entered is incorrect");
-                }
+            window.location = "gutschein.html";
+            alert("Ich liebe dich!");
         }
+    else if(hash===1215)
+        {
+            window.location = "essen.html";
+            alert("Ich liebe dich!");
+        }
+    else
+        {
+            alert("The code you've entered is incorrect");
+        }
+}
+
+function simpleHash(str)
+{
+    let hash = 0;
+    for (i = 0;i < str.length; i++) {
+        hash += str.charCodeAt(i);
+    }
+    return hash;
+}
