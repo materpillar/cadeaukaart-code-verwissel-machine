d0f97ca2ab93f777726a44063133834a580abd17
venhaus <paul@ven.haus>
2018-12-21 12:56:10 -0600

    Formatting changes

--- gutschein.js
+++ gutschein.js
@@ -1,27 +1,28 @@
 function submit()
 {
-    var cd=document.forms["myform"]["code"].value;
+    var cd = document.forms["myform"]["code"].value;
     let hash = simpleHash(cd);
-    if(hash===871)
-        {
-            window.location = "gutschein.html";
-            alert("Ich liebe dich!");
-        }
-    else if(hash===1215)
-        {
-            window.location = "essen.html";
-            alert("Ich liebe dich!");
-        }
+    if(hash === 871)
+    {
+        window.location = "gutschein.html";
+        alert("Ich liebe dich!");
+    }
+    else if(hash === 1215)
+    {
+        window.location = "essen.html";
+        alert("Ich liebe dich!");
+    }
     else
-        {
-            alert("The code you've entered is incorrect");
-        }
+    {
+        alert("The code you've entered is incorrect");
+    }
 }
 
 function simpleHash(str)
 {
     let hash = 0;
-    for (i = 0;i < str.length; i++) {
+    for (i = 0;i < str.length; i++)
+    {
         hash += str.charCodeAt(i);
     }
     return hash;
